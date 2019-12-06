a5fb9f54a52c2b367fc940b6302d75707a7846e3
MelJack1966 <46384386+MelJack1966@users.noreply.github.com>
2019-12-06 17:58:38 -0600

    Update Employee.java

--- Mail-System/src/Employee_System/Employee.java
+++ Mail-System/src/Employee_System/Employee.java
@@ -10,8 +10,13 @@ public class Employee {
 	private String user;
 	private String password;
 	
-
-
+	/**
+	 * description: creates constructor 
+	 * parameters: String firstName, String lastName, String email, String user, String password
+	 * precondition: is called 
+	 * postcondition: creates constructor 
+	 * throws: none
+	 */
 	public Employee(String firstName, String lastName, String email, String user, String password) {
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -19,7 +24,14 @@ public class Employee {
 		this.user = user;
 		this.password = password;
 	}
-
+	
+	/**
+	 * description: creates constructor 
+	 * parameters: none
+	 * precondition: is called 
+	 * postcondition: creates constructor 
+	 * throws: none
+	 */
 	public Employee() {
 		this.firstName = null;
 		this.lastName = null;
@@ -28,42 +40,112 @@ public class Employee {
 		this.email = null;
 	}
 
+	/**
+	 * description: gets user 
+	 * parameters: none
+	 * precondition: is called 
+	 * postcondition: returns user 
+	 * throws: none
+	 */
 	public String getUser() {
 		return user;
 	}
 
+	/**
+	 * description: sets user to this.user 
+	 * parameters: String user
+	 * precondition: is called 
+	 * postcondition: sets user 
+	 * throws: none
+	 */
 	public void setUser(String user) {
 		this.user = user;
 	}
 
+	/**
+	 * description: returns password 
+	 * parameters: none
+	 * precondition: is called 
+	 * postcondition: returns password 
+	 * throws: none
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * description: sets password 
+	 * parameters: String password
+	 * precondition: is called 
+	 * postcondition: sets password to this.password 
+	 * throws: none
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+	/**
+	 * description: returns email 
+	 * parameters: none
+	 * precondition: is called 
+	 * postcondition: returns email 
+	 * throws: none
+	 */
 	public String getEmail() {
 		return email;
 	}
 
+	/**
+	 * description: sets email 
+	 * parameters: String email
+	 * precondition: is called 
+	 * postcondition: sets email 
+	 * throws: none
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
+	/**
+	 * description: returns first name 
+	 * parameters: none
+	 * precondition: is called 
+	 * postcondition: returns first name 
+	 * throws: none
+	 */	
 	public String getFirstName() {
 		return firstName;
 	}
 
+	/**
+	 * description: sets first name 
+	 * parameters: String firstName
+	 * precondition: is called 
+	 * postcondition: sets firstName to this.firstName
+	 * throws: none
+	 */
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
 
+	/**
+	 * description: gets last name 
+	 * parameters: none
+	 * precondition: is called 
+	 * postcondition: returns lastName 
+	 * throws: none
+	 */
 	public String getLastName() {
 		return lastName;
 	}
 
+	/**
+	 * description: sets last name 
+	 * parameters: String lastName
+	 * precondition: is called 
+	 * postcondition: sets last name to this.lastName
+	 * throws: none
+	 */
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
