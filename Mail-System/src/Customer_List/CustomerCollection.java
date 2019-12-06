6c69f53a0e545aad8419e689ff0e075013eede6a
MelJack1966 <46384386+MelJack1966@users.noreply.github.com>
2019-12-06 17:24:26 -0600

    Update CustomerCollection.java

--- Mail-System/src/Customer_List/CustomerCollection.java
+++ Mail-System/src/Customer_List/CustomerCollection.java
@@ -7,6 +7,13 @@ public class CustomerCollection {
 	
 	ArrayList <Customer> list = new ArrayList<Customer>();
 	
+	/**
+	 * description: creates route by sort of zipCode 
+	 * parameters: ArrayList<Customer> list
+	 * precondition: is called 
+	 * postcondition: builds sorted route 
+	 * throws: none
+	 */
 	public void route(ArrayList<Customer> list) {
 		Collections.sort(list, new SortByZipcode());
 		
