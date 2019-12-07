a62209acfc72badc642d79090f9193b3b5f3e7ed
MelJack1966 <46384386+MelJack1966@users.noreply.github.com>
2019-12-06 19:47:53 -0600

    Update Authenticator.java

--- Mail-System/src/login_system/Authenticator.java
+++ Mail-System/src/login_system/Authenticator.java
@@ -6,11 +6,29 @@ public class Authenticator {
 	private Token t;
 	private EmployeeList list;
 
+	/**
+	 * Method name: Authenticator()
+	 * Heading: public Authenticator(Token t, EmployeeList el) 
+	 * Description: to create the constructor of Authenticator
+	 * Parameters: Token t, EmployeeList el
+	 * Precondition: the contructor is called
+	 * Postcondition: creates the constructor
+	 * Throws list: N/A
+	 */
 	public Authenticator(Token t, EmployeeList el) {
 		this.t = t;
 		this.list = el;
 	}
-
+	
+	/**
+	 * Method name: authenticate()
+	 * Heading: public boolean authenticate() 
+	 * Description: to checks in userCheck and passCheck is true
+	 * Parameters: none
+	 * Precondition: if userCheck and passCheck both are true
+	 * Postcondition: returns true or false
+	 * Throws list: N/A
+	 */
 	public boolean authenticate() {
 		boolean userCheck = list.checkUsername(t.getUser());
 		boolean passCheck = list.checkPassword(t.getPword());
