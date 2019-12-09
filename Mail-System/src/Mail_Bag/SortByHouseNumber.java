d181920b8cd20b12c6b17cc70f78c32816d9d50f
MelJack1966 <46384386+MelJack1966@users.noreply.github.com>
2019-12-09 10:42:26 -0600

    Update SortByHouseNumber.java

--- Mail-System/src/Mail_Bag/SortByHouseNumber.java
+++ Mail-System/src/Mail_Bag/SortByHouseNumber.java
@@ -2,6 +2,13 @@ package Mail_Bag;
 
 import java.util.Comparator;
 
+/**
+* description: compares housenumbers of two different customers 
+* parameters: none
+* precondition: is called 
+* postcondition: retuns the comparison of customer1 and customer2 
+* throws: none
+*/
 public class SortByHouseNumber implements Comparator <Mail> {
 	@Override
 	public int compare(Mail o1, Mail o2) {
