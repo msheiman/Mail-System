7f23ecca09b00de50f31acc121ee1edad1846b20
MelJack1966 <46384386+MelJack1966@users.noreply.github.com>
2019-12-09 10:45:25 -0600

    Update SortByZipcode.java

--- Mail-System/src/Mail_Bag/SortByZipcode.java
+++ Mail-System/src/Mail_Bag/SortByZipcode.java
@@ -2,6 +2,13 @@ package Mail_Bag;
 
 import java.util.Comparator;
 
+/**
+* description: compares zipcode of two different customers 
+* parameters: none
+* precondition: is called 
+* postcondition: retuns the comparison of customer1 and customer2 
+* throws: none
+*/
 public class SortByZipcode implements Comparator <Mail> {
 	@Override
 	public int compare(Mail o1, Mail o2) {
